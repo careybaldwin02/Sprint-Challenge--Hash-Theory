d1b6ac48742a6225084b7e99c2509f2782fa495e
Carey <careycbaldwin@gmail.com>
2018-11-16 12:55:38 -0500

    worked on first part of exercise 1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -9,7 +9,21 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
+  LinkedPair *current_pair;
+  if(length>ht->capacity){
+    printf("Too many values entered, cannot exceed, %d", ht->capacity);
+  }
+
+  //place the weights into the hash table as the value in pair
+  for(int i = 0; i < ht->capacity; i++){
+    current_pair = ht->storage[i]; //maybe use create_pair function?
+    while(current_pair->next != NULL){
+      hash_table_insert(ht, current_pair->key, weights[i]);
+      current_pair = current_pair -> next;
+    }
+  }
+
+  
 
   return NULL;
 }
@@ -22,3 +36,19 @@ void print_answer(Answer *answer)
     printf("NULL\n");
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+
+  if (answer_4->index_1 != 6)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+  if (answer_4->index_2 != 2)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+
+  return 0;
+}
+#endif
